1e68873323d934fab8e68d30ecaa381708c57ec6
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-08-15 10:49:15 +0530

    add navbar

---
+++ Frontend/src/Components/NavBar.jsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: "10%",
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+const StyledLogin = styled(Button)(({ theme }) => ({
+  color: "#CC9200",
+  border: " 1px solid #CC9200",
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    width: "20ch",
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  "&:hover": {
+    backgroundColor: "#CC9200",
+    color: "white",
+  },
+}));
+
+const StyledAdd = styled(Button)(({ theme }) => ({
+  marginLeft: "30px",
+  backgroundColor: "#CC9200",
+  border: "1px solid #CC9200",
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    width: "20ch",
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+  "&:hover": {
+    color: "#CC9200",
+  },
+}));
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: "#040404",
+}));
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Home
+          </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Book Now
+          </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            My Bookings{" "}
+          </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            About Us{" "}
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button></Button>
+            <StyledLogin>Login</StyledLogin>
+            <StyledAdd>Register</StyledAdd>
+          </Box>
+        </Toolbar>
+      </StyledAppBar>
+    </Box>
+  );
+}
---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
