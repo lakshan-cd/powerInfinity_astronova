377f067f382a315832fcb6720284fa8f4efc9e11
UserSampath <nalakasampathsmp@gmail.com>
2023-08-16 19:24:45 +0530

    added trip shedule page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import TripSchedule from "./Pages/TripSchedule";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<TripSchedule />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/src/Pages/TripSchedule.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+
+const TripSchedule = () => {
+  return (
+    <div>
+      <NavBar />
+      TripSchedule
+    </div>
+  );
+};
+
+export default TripSchedule;
