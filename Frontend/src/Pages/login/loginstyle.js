2c1ab81e652f6f911043fe8f1a42bcf496f1aa3f
GeshanPasindu <116612958+GeshanPasindu@users.noreply.github.com>
2023-08-16 19:23:07 +0530

    login styled

--- Frontend/src/Pages/login/loginstyle.js
+++ Frontend/src/Pages/login/loginstyle.js
@@ -2,17 +2,35 @@ import background from '../../assets/images/background.png'
 const heading ={
     fontSize: 50,
     Type: "center",
-    backgroundImage:'url()',
     marginTop:'50px',
+    
+    
    
     
 }
+const headingBox= {
+    display: { 
+        xs: "flex", 
+        md: "flex" ,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+}
 
 const text ={
-marginBottom:"20px"    
+    display: { 
+        xs: "flex", 
+        md: "flex" ,
+        alignItems: "flex-start",
+        
+    },
+marginLeft:"0px",
+marginTop:"20px"   
 }
 const main ={
     height:'100vh',
+    color:"white",
+    background:"black"
     
 }
 
@@ -24,21 +42,44 @@ const grid2 = {
 }
 
 const boxmain = {
-    // flexDirection:'column',
-    alignItems: 'center',
+    
+    display: "flex",
+    flexDirection:"column",
+      alignItems: "center",
+      
 }
 
 const formbox ={
-    // display: "flex",
-    // flexDirection:"row",
-    //   alignItems: "center",
-      
+   
+  
     marginTop: '20px',
-    padding:"0px"
+    
 
 }
 const textfield = {
-    marginTop:"10px",
+
+    minWidth:{sm:"200px",md:"500px"},
+    opacity:"0.6",
+    // padding: theme.spacing(0, 0),
+  marginRight: "10px",
+  marginTop: "10px",
+  borderRadius: 10,
+  backgroundColor: "#F7F1F1",
+  height: "50px",
+  "&:hover": {
+    height: "40px",
+    "& fieldset": {
+      borderColor: "transparent", // Change the border color on hover
+    },
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "transparent", // Change the default border color
+    },
+    "&:hover fieldset": {
+      borderColor: "transparent", // Change the border color on hover
+    },
+  },
     
     
     
@@ -46,10 +87,12 @@ const textfield = {
 const loginbutton ={
     
     marginTop: "40px",
-    backgroundColor:"#CC9200",
+    minWidth:"200px",
+    backgroundColor:"#CC9200", 
     "&:hover": {
         backgroundColor: "#CC9200",
         color: "white",
+        border: " 1px solid #CC9200"
       },
       border: " 1px solid #CC9200",
       
@@ -63,4 +106,4 @@ const buttondiv={
 
 
 
-export {heading,main,text,grid2,grid1,boxmain,formbox,textfield,loginbutton}
+export {heading,main,text,grid2,grid1,boxmain,formbox,textfield,loginbutton,headingBox}
