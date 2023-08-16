b641cc38b2c2057ce2c54dd9df99b24b42fea03c
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-08-16 07:42:34 +0530

    add text

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
