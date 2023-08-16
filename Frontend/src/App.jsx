fd37c828f0bcec0545439ad7a685d91fea59deaf
UserSampath <nalakasampathsmp@gmail.com>
2023-08-17 00:32:20 +0530

    calender part completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import TripSchedule from "./Pages/TripSchedule";
+import TripSchedule from "./Pages/tripSchedule/TripSchedule";
 import BookingForm from "./Components/Booking/BookingForm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<TripSchedule />} />
         <Route path="/addreservation" element={<BookingForm />} />
-
       </Routes>
     </BrowserRouter>
   );
