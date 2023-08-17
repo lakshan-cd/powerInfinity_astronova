5e2bab13f943e8339b04c3c80887e63442735bd8
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-08-17 08:55:20 +0530

    add forget password

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -83,7 +83,9 @@ const SendEmail = async (req, res) => {
         from: "powerinfinitypow@gmail.com",
         to: email,
         subject: "Sending Email for verification",
-        text: `This link is valid for 2 minutes `,
+        text: `This link send Hasthiya IT software project management system 
+        Change your account password(valid for 2 minutes)
+         http://localhost:3000/forgotPassword/${userfind.id}/${setusertoken.verifytoken}`,
       };
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -102,11 +104,53 @@ const SendEmail = async (req, res) => {
   }
 };
 
+const forgotpassword = async (req, res) => {
+  const { id, token } = req.params;
+  try {
+    const validuser = await User.findOne({ _id: id, verifytoken: token });
+    const verifyToken = jwt.verify(token, keysecret);
+
+    if (validuser && verifyToken._id) {
+      res.status(201).json({ status: 201, validuser });
+    } else {
+      res.status(401).json({ status: 401, message: "user not exits" });
+    }
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
+};
+
+const reset = async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+  try {
+    const validuser = await User.findOne({
+      id: id,
+      verifytoken: token,
+    });
+    const verifyToken = jwt.verify(token, keysecret);
+    if (validuser && verifyToken._id) {
+      const newpassword = await bcrypt.hash(password, 12);
+
+      const setnewuserpass = await User.findByIdAndUpdate(
+        { _id: id },
+        { password: newpassword }
+      );
+      setnewuserpass.save();
+      res.status(201).json({ status: 201, setnewuserpass });
+    } else {
+      res.status(401).json({ status: 401, message: "user not exits" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 //Check the user token
 
 module.exports = {
   signup,
   login,
-
   SendEmail,
+  forgotpassword,
+  reset,
 };
