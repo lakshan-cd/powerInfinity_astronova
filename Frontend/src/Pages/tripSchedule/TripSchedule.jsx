c47ace4a9462dcf6ba5ae620519ee46f451b1e46
UserSampath <nalakasampathsmp@gmail.com>
2023-08-18 04:02:45 +0530

    time table sort by date complete

--- Frontend/src/Pages/tripSchedule/TripSchedule.jsx
+++ Frontend/src/Pages/tripSchedule/TripSchedule.jsx
@@ -20,6 +20,18 @@ const TripSchedule = () => {
         .then((res) => {
           console.log("tt", res.data);
           setTimetableData(res.data);
+          if (res.data.length != 0) {
+            let currentDate = formatDate(new Date());
+            for (const item of res.data) {
+              if (item._id >= currentDate) {
+                console.log("dd", item._id);
+                scrollToItem(item._id);
+
+                break;
+              }
+              console.log(item._id);
+            }
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -27,7 +39,18 @@ const TripSchedule = () => {
     };
 
     getTimetableDetails();
+
+    console.log(formatDate(new Date()));
   }, []);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
 
   useEffect(() => {
     const refMap = {};
@@ -37,7 +60,6 @@ const TripSchedule = () => {
     if (carouselRef.current) {
       carouselRef.current.refMap = refMap;
     }
-    scrollToItem("2023-08-12");
   }, [timetableData]);
 
   const breakPoints = [
@@ -46,22 +68,44 @@ const TripSchedule = () => {
       itemsToShow: 1,
     },
     {
-      width: 500,
+      width: 340,
+      itemsToShow: 2,
+    },
+    {
+      width: 540,
       itemsToShow: 3,
     },
     {
-      width: 760,
-      itemsToShow: 5,
+      width: 660,
+      itemsToShow: 4,
     },
     {
-      width: 1200,
-      itemsToShow: 6,
+      width: 780,
+      itemsToShow: 5,
     },
   ];
 
   const onChange = (date) => {
-    console.log(date);
+    // console.log(formatDate(date));
     setDate(date);
+    if (timetableData.length != 0) {
+      let currentDate = formatDate(date);
+      for (const item of timetableData) {
+        if (item._id == currentDate) {
+          console.log("dd", item._id);
+          scrollToItem(item._id);
+          break;
+        } else if (item._id > currentDate) {
+          alert(
+            "There are no trips scheduled for that day. Here are the nearest trips based on your search date"
+          ); 
+          console.log("no document", item._id);
+          scrollToItem(item._id);
+          break;
+        }
+        // console.log(item._id);
+      }
+    }
   };
   const myArrow = ({ type, onClick, isEdge }) => {
     const pointer = type === consts.PREV ? "<" : ">";
@@ -88,6 +132,105 @@ const TripSchedule = () => {
           <div className="filterBox">
             <div style={{}}>
               <Calendar onChange={onChange} value={date} />
+            </div>
+
+            <div className="optBtn">
+              <div>
+                <label className="optBtn-label">From :</label>
+                <select className="filter-dropDown">
+                  <option
+                    value=""
+                    disabled
+                    selected
+                    style={{ display: "none" }}>
+                    Select a planet
+                  </option>
+                  <option className="filter-dropDown-option" value="Mars">
+                    Mars
+                  </option>
+                  <option className="filter-dropDown-option" value="Venus">
+                    Venus
+                  </option>
+                  <option className="filter-dropDown-option" value="Jupiter">
+                    Jupiter
+                  </option>
+                  <option className="filter-dropDown-option" value="Saturn">
+                    Saturn
+                  </option>
+                  <option className="filter-dropDown-option" value="Mercury">
+                    Mercury
+                  </option>
+                  <option className="filter-dropDown-option" value="Neptune">
+                    Neptune
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            <div className="optBtn">
+              <div>
+                <label className="optBtn-label">To :</label>
+                <select className="filter-dropDown">
+                  <option
+                    value=""
+                    disabled
+                    selected
+                    style={{ display: "none" }}>
+                    Select a planet
+                  </option>
+                  <option className="filter-dropDown-option" value="Mars">
+                    Mars
+                  </option>
+                  <option className="filter-dropDown-option" value="Venus">
+                    Venus
+                  </option>
+                  <option className="filter-dropDown-option" value="Jupiter">
+                    Jupiter
+                  </option>
+                  <option className="filter-dropDown-option" value="Saturn">
+                    Saturn
+                  </option>
+                  <option className="filter-dropDown-option" value="Mercury">
+                    Mercury
+                  </option>
+                  <option className="filter-dropDown-option" value="Neptune">
+                    Neptune
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            <div className="optBtn">
+              <div>
+                <label className="optBtn-label">Mode :</label>
+                <select className="filter-dropDown">
+                  <option
+                    value=""
+                    disabled
+                    selected
+                    style={{ display: "none" }}>
+                    Select a planet
+                  </option>
+                  <option className="filter-dropDown-option" value="Mars">
+                    Mars
+                  </option>
+                  <option className="filter-dropDown-option" value="Venus">
+                    Venus
+                  </option>
+                  <option className="filter-dropDown-option" value="Jupiter">
+                    Jupiter
+                  </option>
+                  <option className="filter-dropDown-option" value="Saturn">
+                    Saturn
+                  </option>
+                  <option className="filter-dropDown-option" value="Mercury">
+                    Mercury
+                  </option>
+                  <option className="filter-dropDown-option" value="Neptune">
+                    Neptune
+                  </option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
