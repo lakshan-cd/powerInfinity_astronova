343d60daddfc17d516caafc351f26334712b8fd3
Chanaka <chanaka7518@gmail.com>
2023-08-17 20:30:31 +0530

    Modified bookingModal

--- Backend/models/bookingModel.js
+++ Backend/models/bookingModel.js
@@ -1,39 +1,27 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const Schema = mongoose.Schema;
+const { SelectClass } = require("../utils/Constants");
 
 const bookingSchema = new Schema(
   {
-    // userId is an identifier for our customer that booked the ticket
-    user_id: {
-      type: String,
-      required: true,
-    },
-    // field contains the ID of the customer who made the payment. It
-    customer_id: {
-      type: String,
-      required: true,
-    },
-
-    // to store passenger ids  from user database
-    passsenger_ids: {
-      type: [String],
-      required: true,
-    },
-    distance: {
-      type: String,
+    trip: {
+      type: Schema.Types.ObjectId,
+      ref: "Trip",
     },
     class: {
       type: String,
+      enum: [
+        SelectClass.FirstClass,
+        SelectClass.BuisnessClass,
+        SelectClass.EconomyClass,
+      ],
     },
-    total_payment: {
-      type: Number,
+    BookingDate: {
+      type: Date,
       required: true,
     },
-    payment_status: { type: String, default: "incomplete" },
-    payment_intent: { type: String },
   },
   { timestamps: true }
 );
-
 module.exports = mongoose.model("Booking", bookingSchema);
