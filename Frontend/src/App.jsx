4ad22ef4b89429f8d5b5c1b79aa7160b08a73ad0
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2023-08-17 08:05:42 +0530

    pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TripSchedule from "./Pages/TripSchedule";
 import Home from "./Pages/Home";
 import Payment from "./Pages/Payment";
+import Signup from "./Pages/Signup";
+import BookingForm from "./Components/Booking/BookingForm";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +14,8 @@ function App() {
         <Route exact path="/Payment" element={<Payment />} />
 
         <Route  path="Payment" element={<Payment />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/addreservation" element={<BookingForm />} />
 
       </Routes>
     </BrowserRouter>
@@ -18,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
